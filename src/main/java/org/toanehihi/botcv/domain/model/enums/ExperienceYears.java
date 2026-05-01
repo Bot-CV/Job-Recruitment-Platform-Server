34235d270387888d81a594eafea0db4e184e745e
toanehihi <hocongtoan987@gmail.com>
2026-05-02 03:33:13 +0700

    feat: add CompanySize, ExperienceYears, VerificationStatus enums; update ResourceType and EmploymentType

---
+++ src/main/java/org/toanehihi/botcv/domain/model/enums/ExperienceYears.java
@@ -0,0 +1,6 @@
+package org.toanehihi.botcv.domain.model.enums;
+
+public enum ExperienceYears {
+    NO_EXPERIENCE, UNDER_1_YEAR, ONE_YEAR, TWO_YEARS,
+    THREE_YEARS, FOUR_YEARS, FIVE_YEARS, OVER_5_YEARS;
+}
