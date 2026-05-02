c434134cf6e105211a8ca655551b4558e388efaa
toanehihi <hocongtoan987@gmail.com>
2026-05-02 12:46:32 +0700

    fix: align Interview (meetingUrl, column annotations) and Role (dateCreated)

--- src/main/java/org/toanehihi/botcv/domain/model/Role.java
+++ src/main/java/org/toanehihi/botcv/domain/model/Role.java
@@ -1,9 +1,11 @@
 package org.toanehihi.botcv.domain.model;
 
 import java.io.Serializable;
+import java.time.OffsetDateTime;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Getter
 @Setter
@@ -15,11 +17,14 @@ import lombok.*;
 @Table(name = "roles")
 public class Role implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "name")
-	private String name;
+    @Column(name = "name")
+    private String name;
 
+    @CreationTimestamp
+    @Column(name = "date_created")
+    private OffsetDateTime dateCreated;
 }
